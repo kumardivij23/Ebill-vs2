6d3948c291087be554053495d30e5a9ef15d91f8
Kumar Divij <75716308+kumardivij23@users.noreply.github.com>
2024-06-18 13:11:42 +0530

    Update sessioncheck.js

--- sessioncheck.js
+++ sessioncheck.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const session = localStorage.getItem('session');
     if (session !== 'loggedin') {
-        alert('You are not logged in!');
+        // alert('You are not logged in!');
         window.location.href = 'login.html';
     } else {
         // document.getElementById('mainContent').classList.remove('hidden');
